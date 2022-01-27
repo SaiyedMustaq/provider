d0ae26fac4a4864e1b7c9740fe7873460a11a694
Mustaq_Saiyed <mustaq.saiyed@imperoit.com>
2022-01-27 11:30:55 +0530

    struct set

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,12 @@
+import 'dart:ui';
+
 import 'package:flutter/material.dart';
+import 'package:internet_connection_checker/internet_connection_checker.dart';
+import 'package:provider/provider.dart';
+
+import 'provider/paginvation/PaginationProviderPage.dart';
+import 'provider/paginvation/PhotoProvider.dart';
+import 'provider/simpleProvider/SimpleProvider.dart';
 
 void main() {
   runApp(const MyApp());
@@ -8,12 +16,33 @@ class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
+    return StreamProvider<InternetConnectionStatus>(
+      initialData: InternetConnectionStatus.connected,
+      create: (context) => InternetConnectionChecker().onStatusChange,
+      child: MultiProvider(
+        providers: [
+          ChangeNotifierProvider<PhotoProvider>(
+            create: (_) => PhotoProvider(),
+          ),
+        ],
+        child: MaterialApp(
+          debugShowCheckedModeBanner: false,
+          title: 'Flutter Demo',
+          theme: ThemeData(
+            primarySwatch: Colors.blue,
+            listTileTheme: ListTileThemeData(
+              tileColor: Colors.blue,
+              contentPadding: EdgeInsets.all(5),
+              horizontalTitleGap: 1.2,
+              dense: true,
+              textColor: Colors.white,
+              enableFeedback: true,
+              selectedColor: Colors.red,
+            ),
+          ),
+          home: const MyHomePage(title: 'Flutter Demo Home Page'),
+        ),
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
     );
   }
 }
@@ -28,17 +57,59 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+  final listTileStyle = TextStyle(
+    fontSize: 15,
+    color: Colors.white,
+  );
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text(widget.title),
       ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: const <Widget>[],
-        ),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.start,
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: <Widget>[
+          const SizedBox(height: 5),
+          ListTile(
+            onTap: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) => SimpleProvider()),
+              );
+            },
+            title: Text(
+              'Simple provider',
+              style: listTileStyle,
+            ),
+            trailing: const Icon(
+              Icons.arrow_right,
+              color: Colors.white,
+              size: 30,
+            ),
+          ),
+          const SizedBox(height: 5),
+          ListTile(
+            trailing: const Icon(
+              Icons.arrow_right,
+              color: Colors.white,
+              size: 30,
+            ),
+            onTap: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(
+                    builder: (context) => const PaginationProviderPage()),
+              );
+            },
+            title: Text(
+              'Simple provider',
+              style: listTileStyle,
+            ),
+          )
+        ],
       ),
       // This trailing comma makes auto-formatting nicer for build methods.
     );
---
+++ lib/provider/paginvation/PhotoProvider.dart
@@ -0,0 +1,59 @@
+import 'dart:developer';
+
+import 'package:dio/dio.dart';
+import 'package:flutter/cupertino.dart';
+
+import '../../network/apiClient.dart';
+import 'PhotoModel.dart';
+
+//git add
+class PhotoProvider extends ChangeNotifier {
+  int _page = 1;
+  int get page => _page;
+
+  bool _isLoading = true;
+  bool get isLoading => _isLoading;
+
+  bool _isNetworkAvailable = true;
+  bool get isNetworkAvailable => _isNetworkAvailable;
+
+  List<Photos> _photos = <Photos>[];
+  List<Photos> get photos => _photos;
+
+  set page(int pageIndex) {
+    _page = pageIndex;
+    notifyListeners();
+  }
+
+  set loading(bool isLoad) {
+    _isLoading = isLoad;
+    notifyListeners();
+  }
+
+  set photos(List<Photos> value) {
+    _photos = value;
+    notifyListeners();
+  }
+
+  Future<void> callPhotoApi() async {
+    try {
+      await apiClient().getPhotos(_page, 'nature').then((response) {
+        _page = _page + 1;
+        addPhotosToList(PhotosModel.fromJson(response).photos!);
+      });
+    } on DioError catch (e) {
+      if (e.response?.statusCode == 404) {
+        log('${e.response?.statusCode}', error: "Status Code");
+      } else {
+        log(e.message, error: "Message");
+      }
+    }
+    notifyListeners();
+  }
+
+  void addPhotosToList(List<Photos> photoData) {
+    _photos.addAll(photoData);
+    _isLoading = false;
+    notifyListeners();
+  }
+}
